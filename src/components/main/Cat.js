e6c853194ca81e801c66a228ae7615e74abc3b07
DDuongNguyen <duong.nguyen4991@gmail.com>
2019-07-30 10:21:10 -0400

    schrodinger cat

--- src/components/main/Cat.js
+++ src/components/main/Cat.js
@@ -3,22 +3,37 @@ import React, { Component } from 'react';
 class Cat extends Component {
 
 
-  render() {
-
+conditionalCat = () => {
+  if (this.props.className === 'side-bar-cat') {
     return (
-      <div className={this.props.className} >
-      <img
-      onClick={()=> {this.props.handleCatClick(this.props.cat)}}
-      src={this.props.cat.cat_img}
-      alt='this a cat'/>
-      {(this.props.className==='side-bar-cat')?
-      <h1> {this.props.cat.name} </h1>: null
-      }
-
+      <div className = {this.props.className}>
+      <img onClick = {() => {this.props.handleCatClick(this.props.cat)}}
+      src = {this.props.cat.cat_img}
+      alt = 'this a cat'/>
+      <h1>{this.props.cat.name}</h1>
       </div>
     )
   }
 
-}
+
+  else if (this.props.className === 'cafe-cat'){
+    return (
+      <div className = {this.props.className} >
+      <img onClick = {() => {this.props.handleCatClick(this.props.cat)}}
+      src = {this.props.cat.cat_img}
+      alt = 'this a cafe cat' />
+      <h1>{this.props.cat.personality}</h1>
+      </div>
+      )
+  }}
+
+  render(){
+    return(
+      <cat>
+      {this.conditionalCat()}
+      </cat>
+    )}
+
+  }
 
 export default Cat;
